8a3e1de807f7f6a6d48820e53c6de1b8df6c8bb1
Joword <23089538@qq.com>
2023-03-07 23:09:00 +0800

    init project

---
+++ snv-common/src/main/java/com/snv/Main.java
@@ -0,0 +1,13 @@
+package com.snv;
+
+/**
+ * @author Joword
+ * @date: ${DATE} ${HOUR}:${MINUTE}
+ * @version:
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ snv-manager/src/main/java/com/joword/snv/Main.java
@@ -0,0 +1,13 @@
+package com.joword.snv;
+
+/**
+ * @author Joword
+ * @date: ${DATE} ${HOUR}:${MINUTE}
+ * @version:
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
